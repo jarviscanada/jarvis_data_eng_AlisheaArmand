0a6db968ad58148cae38e740b18fb619629f6b27
Alishea Armand <aleashedadventure@gmail.com>
2022-05-13 15:49:39 +0000

    fixed harware variables to work with insert statement

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -13,22 +13,22 @@ if [ $# -ne  5 ]; then
   exit 1
 fi
 
-lscpu_out='lscpu'
+#lscpu_out='lscpu'
 hostname=$(hostname -f)
 
 #Retrieve hardware specification variables
-cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture=$(echo "$lscpu_out"  | egrep "Architecture" | awk '{print $2}' | xargs)
-cpu_model=$(echo "$lscpu_out"  | egrep "\bModel\s\bname" | awk '{print $3, $4, $5}' | xargs)
-cpu_mhz=$(echo "$lscpu_out"  | egrep "\bCPU\s\MHz" | awk '{print $3}' | xargs)
-l2_cache=$(echo "$lscpu_out"  | egrep "\bL2\s\bcache" | awk '{print $3}' | xargs)
+cpu_number=$(lscpu  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(lscpu | egrep "Architecture" | awk '{print $2}' | xargs)
+cpu_model=$(lscpu | egrep "\bModel\s\bname" | awk '{print $3, $4, $5}' | xargs)
+cpu_mhz=$(lscpu  | egrep "\bCPU\s\MHz" | awk '{print $3}' | xargs)
+l2_cache=$(lscpu | egrep "\bL2\s\bcache" | awk '{print $3}' | xargs)
 total_mem=$(grep "^MemTotal" /proc/meminfo | awk '{ print $2 }')
 timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1| xargs)
 
-#Insert server usage data into host_inof table
-insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp');"
+#Insert server usage data into host_info table
+insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) VALUES ('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp');"
 
-#set up env var for pql cmd
+#set up environment variable for pql cmd
 export PGPASSWORD=$psql_password
 
 #Insert data into database
