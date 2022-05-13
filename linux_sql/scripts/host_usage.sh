16c854bcd0aa4fdc522215be69684545dcfba7c5
Alishea Armand <aleashedadventure@gmail.com>
2022-05-13 18:09:51 +0000

    fixed host_id syntax error

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -29,7 +29,8 @@ timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1| xargs)
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 
 #Insert server usage data into host_usage table
-insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES('$timestamp', '$host_id', '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available');"
+insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
+VALUES('$timestamp', $host_id, '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', $disk_available)";
 
 #set up env var for pql cmd
 export PGPASSWORD=$psql_password
