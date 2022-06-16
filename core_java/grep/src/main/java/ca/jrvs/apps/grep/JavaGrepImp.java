140fb2686839b5e8e326723f2a033dc94b4ddeaa
Alishea Armand <104445847+syngnathidae@users.noreply.github.com>
2022-06-16 14:44:44 -0600

    added JavaGrep interface, and JavaGrepImp and JavaGrepI classes

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,97 @@
+package grep.src.main.java.ca.jrvs.apps.grep;
+
+//import java.util.logging.Logger;
+import org.slf4j.Logger;
+import  org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JavaGrepImp implements JavaGrep{
+
+    final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
+
+    private String regex;
+    private String rootPath;
+    private String outFile;
+
+    @Override
+    public String getRegex() {
+        return regex;
+    }
+
+    @Override
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    @Override
+    public String getRootPath() {
+        return rootPath;
+    }
+
+    @Override
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    @Override
+    public String getOutFile() {
+        return outFile;
+    }
+
+    @Override
+    public void setOutFile(String outFile) {
+        this.outFile = outFile;
+    }
+
+
+
+    @Override
+    public List<File> listFiles(String rootDir) {
+        return null;
+    }
+
+    @Override
+    public List<String> readLines(File inputFile) {
+        return null;
+    }
+
+    @Override
+    public boolean containsPattern(String line) {
+        return false;
+    }
+
+    @Override
+    public void writeToFile(List<String> lines) throws IOException {
+
+    }
+
+    @Override
+    public void process() throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("USAGE: JavaGrep regex roothPath outFile");
+        }
+
+        //use default logger config
+        //BasicConfigurator.configure();
+
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRegex(args[1]);
+        javaGrepImp.setRegex(args[2]);
+
+        try {
+            javaGrepImp.process();
+        } catch (Exception ex) {
+            javaGrepImp.logger.error("Error: Unable to process", ex);
+        }
+
+    }
+
+}
