a0882575fbf5edf1c05a39ac023b8d46a635fc27
Alishea Armand <aleashedadventure@gmail.com>
2022-07-06 21:00:29 +0000

    Creating Data video

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,5 +1,7 @@
-package ca.jrvs.apps.jdbc;
+package main.java.ca.jrvs.apps.jdbc;
 
+import ca.jrvs.apps.jdbc.Customer;
+import ca.jrvs.apps.jdbc.CustomerDAO;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -7,14 +9,21 @@ import java.sql.Statement;
 
 public class JDBCExecutor {
     public static void main(String[] args) {
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "password");
+        ca.jrvs.apps.jdbc.DatabaseConnectionManager dcm = new ca.jrvs.apps.jdbc.DatabaseConnectionManager("localhost", "hplussport", "postgres", "password");
         try {
             Connection connection = dcm.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT  COUNT (*) FROM CUSTOMER");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt(1));
-            }
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+            Customer customer = new Customer();
+            customer.setFirstName("George");
+            customer.setFirstName("Washington");
+            customer.setEmail("george.washington@wh.gov");
+            customer.setPhone("(555) 555-6543");
+            customer.setAddress("1234 Main st");
+            customer.setCity("Mount Vernon");
+            customer.setState("VA");
+            customer.setZipCode("22121");
+
+            customerDAO.create(customer);
         } catch (SQLException e) {
             e.printStackTrace();
         }
