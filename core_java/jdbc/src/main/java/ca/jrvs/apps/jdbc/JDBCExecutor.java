bcc7129ada9b19c52ff7c2f669f3920bf27c50c2
Alishea Armand <aleashedadventure@gmail.com>
2022-07-07 21:12:00 +0000

    transactions

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class JDBCExecutor {
     public static void main(String... args){
@@ -17,8 +18,8 @@ public class JDBCExecutor {
         try{
             Connection connection = dcm.getConnection();
             OrderDAO orderDAO = new OrderDAO(connection);
-            Order order = orderDAO.findById(1000);
-            System.out.println(order);
+            List<Order> orders = orderDAO.getOrdersForCustomer(789);
+            orders.forEach(System.out::println);
         }catch(SQLException e){
             e.printStackTrace();
         }
